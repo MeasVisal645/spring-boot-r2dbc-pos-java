101a5f25c7b8e315aa24cc0d6beda4a398292a9c
LinKaiz645 <measvathanakvisal@gmail.com>
2026-03-13 14:54:46 +0700

    update:add response to request

--- src/main/java/backend/Service/CategoryService.java
+++ src/main/java/backend/Service/CategoryService.java
@@ -4,6 +4,7 @@ import backend.Dto.CategoryDto;
 import backend.Dto.CategoryProduct;
 import backend.Entities.Category;
 import backend.Utils.PageResponse;
+import backend.Utils.ResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -13,10 +14,10 @@ import reactor.core.publisher.Mono;
 public interface CategoryService {
 
     Flux<CategoryDto> findAll();
-    Mono<Category> findById(Long id);
-    Mono<Category> create(Category category);
-    Mono<Category> update(Category category);
-    Mono<Long> delete(Long id);
+    Mono<ResponseDTO<Category>> findById(Long id);
+    Mono<ResponseDTO<Category>> create(Category category);
+    Mono<ResponseDTO<Category>> update(Category category);
+    Mono<ResponseDTO<Long>> delete(Long id);
 
     Mono<PageResponse<CategoryDto>> findPagination(Integer pageNumber, Integer pageSize, String search, Boolean isActive);
 }
